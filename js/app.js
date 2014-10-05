36cfa4326408084430daac219a468fab8fa2a5ed
Rira Choi <rira.nunu.choi@gmail.com>
2014-10-05 18:06:07 +1100

    css

--- js/app.js
+++ js/app.js
@@ -201,7 +201,7 @@
         image: "http://i.imgur.com/qSXtyDG.png",
         description: "An international news application using geographical location with the world map. USING: Ruby on Rails, jQuery, feedzilla API, TDD with Simplecov and RSpec."
       },{ name:"NSWFR Notification",
-        url:"#",
+        url:"https://github.com/rirachoi/nswfire_zabbix",
         github:"https://github.com/rirachoi/nswfire_zabbix",
         image: "http://i.imgur.com/suv8bt1.png",
         description: "Zabbix Email, SMS and Web Linked Alerting framework. Consists of multiple forms that are browser independent and cross platform. Built in HTML, CSS and jQuery. Utilized by NSW Fire and Rescue for Infrastructure Alerting, Outages and Notifications."
